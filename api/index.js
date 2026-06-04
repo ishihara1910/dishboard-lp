623ede8a0a13c3f1b282b8d3c45c9d9bd64dc4f5
ishihara1910 <ishihara1910@gmail.com>
2026-06-05 00:46:45 +0900

    fix: use getRouter in api handler

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import { createStartHandler, defaultStreamHandler } from "@tanstack/react-start/server";
-import { createRouter } from "../src/router";
+import { getRouter } from "../src/router";
 
 export default createStartHandler({
-  createRouter,
+  createRouter: getRouter,
 })(defaultStreamHandler);
